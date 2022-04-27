1690cc06221add41ef2dcebbe8e1a53445850b4d
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-27 09:57:48 -0600

    rearranging portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -32,29 +32,59 @@ export default function Portfolio() {
           </div>
         </div>
 
-        <div className="weatherTracker">
+        <div className="CacheBandit">
           <div>
             <a
-              href="  https://tracyewilhelm.github.io/weather-tracker/"
+              href="https://cache-bandit.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
             >
-              <img
-                id="weatherTrackerImg"
-                src={weatherTracker}
-                alt="black rectangle with blue words CSS Cheat Sheet"
-              />
-              <p>Weather Tracker</p>
+              <img id="CacheBandit" src={CacheBandit} alt="Cache Bandit Logo" />
+              <p>Cache Bandit</p>
             </a>
-            <p>A way to see the 5-day weather forecast of almost any city</p>
-
+            <p>
+              A full-stack, mobile-first web app that allows a user to monitor
+              their monthly and annual subscriptions. Allows a user to enter
+              data about all of their subscriptions so they can see it all in
+              one location, including monthly cost, due date, and the ability to
+              edit or delete individual subscriptions.
+            </p>
             <p>
               <a
-                href="https://github.com/tracyewilhelm/weather-tracker"
+                href="https://github.com/alexgeis/Subscription-Tracker"
                 target="_blank"
                 rel="noreferrer"
               >
-                GitHub Link: Weather Tracker
+                GitHub Link: Cache Bandit
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="PantryChef">
+          <div>
+            <a
+              href="https://awesome-pantry-chef.herokuapp.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img id="PChefImg" src={PantryChefImg} alt="Pantry Chef Logo" />
+              <p>Pantry Chef</p>
+            </a>
+            <p>
+              A full-stack mobile-first web app that allows a user to check mark
+              items in their pantry and see recipes based on their selection.
+              Allows a user to see their favorite recipes by storing user info
+              and saved recipe ID to a remote database. Technology used includes
+              Bootstrap, Handlebars, Node, JavaScript, JSON, MySql, bcrypt,
+              Express, Sequelize, and 3rd Party API calls.
+            </p>
+            <p>
+              <a
+                href="https://github.com/tracyewilhelm/Pantry_Chef"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Link: Pantry Chef
               </a>
             </p>
           </div>
@@ -91,6 +121,33 @@ export default function Portfolio() {
                 GitHub Link: paterma/BugSpray
               </a>
             </p>
+          </div>
+          <div className="weatherTracker">
+            <div>
+              <a
+                href="  https://tracyewilhelm.github.io/weather-tracker/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  id="weatherTrackerImg"
+                  src={weatherTracker}
+                  alt="black rectangle with blue words CSS Cheat Sheet"
+                />
+                <p>Weather Tracker</p>
+              </a>
+              <p>A way to see the 5-day weather forecast of almost any city</p>
+
+              <p>
+                <a
+                  href="https://github.com/tracyewilhelm/weather-tracker"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub Link: Weather Tracker
+                </a>
+              </p>
+            </div>
           </div>
         </div>
 
@@ -136,63 +193,6 @@ export default function Portfolio() {
                 rel="noreferrer"
               >
                 GitHub Link: Daily-Planner
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="PantryChef">
-          <div>
-            <a
-              href="https://awesome-pantry-chef.herokuapp.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img id="PChefImg" src={PantryChefImg} alt="Pantry Chef Logo" />
-              <p>Pantry Chef</p>
-            </a>
-            <p>
-              A full-stack mobile-first web app that allows a user to check mark
-              items in their pantry and see recipes based on their selection.
-              Allows a user to see their favorite recipes by storing user info
-              and saved recipe ID to a remote database. Technology used includes
-              Bootstrap, Handlebars, Node, JavaScript, JSON, MySql, bcrypt,
-              Express, Sequelize, and 3rd Party API calls.
-            </p>
-            <p>
-              <a
-                href="https://github.com/tracyewilhelm/Pantry_Chef"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub Link: Pantry Chef
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="CacheBandit">
-          <div>
-            <a
-              href="https://cache-bandit.herokuapp.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img id="CacheBandit" src={CacheBandit} alt="Cache Bandit Logo" />
-              <p>Cache Bandit</p>
-            </a>
-            <p>
-              A full-stack, mobile-first web app that allows a user to monitor
-              their monthly and annual subscriptions. Allows a user to enter
-              data about all of their subscriptions so they can see it all in
-              one location, including monthly cost, due date, and the ability to
-              edit or delete individual subscriptions.
-            </p>
-            <p>
-              <a
-                href="https://github.com/alexgeis/Subscription-Tracker"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub Link: Cache Bandit
               </a>
             </p>
           </div>
